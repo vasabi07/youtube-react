2e397799ce000aeead9a09c8e9a1b81ec99c0bf7
vasabi07 <vasanthanarulselvan@gmail.com>
2024-06-23 17:45:16 +0530

    added base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/youtube-react/",
+  base: "/youtube-react/",
   plugins: [react()],
   
 })
